3a7f5a85868013a6a9bd31c96e8a6e92a575527d
natsuki <183352809+K-tal@users.noreply.github.com>
2025-04-06 09:57:05 +0900

    MemoApp Header Styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,39 +3,45 @@ import { View, Text, StyleSheet } from "react-native";
 const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <View>
-        <Text>Memo App</Text>
-        <Text>ログアウト</Text>
-      </View>
-      <View>
-        <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2025年4月6日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-        <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2025年4月6日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-        <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2025年4月6日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
+
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年4月6日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年4月6日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年4月6日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
       <View>
         <Text>＋</Text>
       </View>
@@ -46,8 +52,28 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#ffffff",
+  },
+  header: {
+    backgroundColor: "#E0A9E4",
+    height: 104,
+    justifyContent: "flex-end",
+  },
+  headerInner: {
     alignItems: "center",
+  },
+  headerRight: {
+    position: "absolute",
+    right: 16,
+    bottom: 16,
+    color: "rgba(255, 255, 255, 0.7)",
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: "bold",
+    color: "#ffffff",
   },
 });
 export default Index;
